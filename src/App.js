cc653c23a0004251cf44876944c83540f428787c
john-hoey <john.t.hoey@gmail.com>
2021-04-28 21:10:37 -0400

    first commit. learning react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  // const para = document.createElement("p");
+  // para.textContent = "This is also visible";
+  // document.getElementById("root").append(para);
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <p>This is also visible!</p>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
