5fa35f735edb7a604f00b519bab192c16f232a5e
john-hoey <john.t.hoey@gmail.com>
2021-05-01 01:28:41 -0400

    component organization with new folder structure.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,11 @@
 import ExpenseItem from "./ExpenseItem";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 import "./Expenses.css";
 
 function Expenses(props) {
   return (
-    <Card className="expenses">
+    <Card classname="expenses">
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
